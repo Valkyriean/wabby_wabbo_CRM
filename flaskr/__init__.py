64b33b03ed70ab337be5d7eaa427dfaef107ba7e
Jiachen Li <alexjiachenli@hotmail.com>
2021-09-18 23:08:41 +1000

    Shift to split design of front and back ends

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 from flaskr.setup import login_manager, db
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY='nana7mi',
     )
     app.config['MONGODB_SETTINGS'] = {
         "db": "crm",
@@ -33,12 +33,23 @@ def create_app(test_config=None):
     login_manager.login_view = 'auth.login'
 
     # a simple page that says hello
-    @app.route('/')
-    def hello():
+    @app.route('/', methods= ["GET"])
+    def index():
         return render_template('homepage.html')
 
 
+    @app.route('/js/<filename>', methods=["GET"])
+    def get_js(filename):
+        return send_file('/path/to/js/{0}'.format(filename))
+
+    @app.route('/css/<filename>', methods=["GET"])
+    def get_css(filename):
+        return send_file('/path/to/css/{0}'.format(filename))
+
+    # @app.route('/', methods=["GET"])
+    # def get_index():
+    #     return send_file('index.html')
+
     from . import auth
     app.register_blueprint(auth.bp)
-    
     return app
--- flaskr/auth.py
+++ flaskr/auth.py
@@ -3,27 +3,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 from flaskr.db_models.auth_model import Company
 from flaskr.setup import login_manager
-# for nav bar
-from flask import Flask
-# from flask_bootstrap import Bootstrap
-# from flask_nav import Nav
-# from flask_nav.elements import *
-# from dominate.tags import img
-
-## for nav bar
-# logo = img(src='./static/img/logo.png', height="60", width="100", style="margin-top:-15px")
-
-# topbar = Navbar(logo,
-#                 View('Log In', 'login'),
-#                 View('Live', 'register'),
-#                 View('Programme', 'get_programme'),
-#                 View('Classement', 'get_classement'),
-#                 View('Contact', 'get_contact'),
-#                 )
-
-# registers the "top" menubar
-# nav = Nav()
-# nav.register_element('top', topbar)
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -68,8 +47,7 @@ def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        rememberMe = (request.form['rememberMe'] is 1)
-        
+        rememberMe = (request.form['rememberMe'] == "1")
         error = None 
         user = Company.objects(email=email).first()
         if user is None:

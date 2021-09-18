a10307d1d39dafb8f389dcacd795f8f47a9cd0ef
Jiachen Li <alexjiachenli@hotmail.com>
2021-09-18 14:43:20 +1000

    Update auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -5,25 +5,25 @@ from flaskr.db_models.auth_model import Company
 from flaskr.setup import login_manager
 # for nav bar
 from flask import Flask
-from flask_bootstrap import Bootstrap
-from flask_nav import Nav
-from flask_nav.elements import *
-from dominate.tags import img
+# from flask_bootstrap import Bootstrap
+# from flask_nav import Nav
+# from flask_nav.elements import *
+# from dominate.tags import img
 
 ## for nav bar
-logo = img(src='./static/img/logo.png', height="60", width="100", style="margin-top:-15px")
+# logo = img(src='./static/img/logo.png', height="60", width="100", style="margin-top:-15px")
 
-topbar = Navbar(logo,
-                View('Log In', 'login'),
-                View('Live', 'register'),
-                View('Programme', 'get_programme'),
-                View('Classement', 'get_classement'),
-                View('Contact', 'get_contact'),
-                )
+# topbar = Navbar(logo,
+#                 View('Log In', 'login'),
+#                 View('Live', 'register'),
+#                 View('Programme', 'get_programme'),
+#                 View('Classement', 'get_classement'),
+#                 View('Contact', 'get_contact'),
+#                 )
 
 # registers the "top" menubar
-nav = Nav()
-nav.register_element('top', topbar)
+# nav = Nav()
+# nav.register_element('top', topbar)
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')

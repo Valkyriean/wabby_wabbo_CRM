3c885e7f8f2bcf78b4d6ac50061af0b74ae91a26
Zenan Huang <zenan.huang@student.unimelb.edu.au>
2021-09-23 19:31:34 +1000

    added menu.vue and carousel.vue

---
+++ frontend/src/components/Carousel.vue
@@ -0,0 +1,25 @@
+<template>
+  <a-carousel autoplay>
+    <div><h3>1</h3></div>
+    <div><h3>2</h3></div>
+    <div><h3>3</h3></div>
+    <div><h3>4</h3></div>
+  </a-carousel>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+/* For demo */
+.ant-carousel >>> .slick-slide {
+  text-align: center;
+  height: 160px;
+  line-height: 160px;
+  background: #364d79;
+  overflow: hidden;
+}
+
+.ant-carousel >>> .slick-slide h3 {
+  color: #fff;
+}
+</style>
